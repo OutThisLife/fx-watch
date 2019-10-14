86583a7002b237cfae3d717c4ca477324804f7d3
talasan.nicholson@gmail.com <talasan.nicholson@gmail.com>
2019-10-14 14:20:06 -0500

    quick gal commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 
-const { api_provider } = process.env
+const { API_PROVIDER } = process.env
 
 module.exports = {
   serverRuntimeConfig: {
-    api_provider
+    API_PROVIDER
   }
 }
